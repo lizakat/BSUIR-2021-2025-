6988fabb3be31fa7d3d3812adc35566aec1d7ace
k.vetochka <91129112+lizakat@users.noreply.github.com>
2022-11-21 02:02:42 +0300

    FINAL VERSION

--- programming on high-level languages/laba8_4exception/laba8_4exception.cpp
+++ programming on high-level languages/laba8_4exception/laba8_4exception.cpp
@@ -94,7 +94,7 @@ public:
             for (int i = 0; i < size - 1; i++)
             {
                 array[i] = array[i + 1];
-                // throw(1);
+                // throw 1;
             }
             size--;
             rear--;
